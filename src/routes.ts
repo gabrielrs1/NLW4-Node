117d333ae11505dee45e9a50881e60dac5c4ff33
Gabriell-Ribeiro <gabriellribeiro208@gmail.com>
2021-02-25 18:49:22 -0300

    dia4

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { SendMailController } from "./controllers/SendMailController"
 import { SurveysController } from "./controllers/SurveysController"
 import { UserController } from "./controllers/UserController"
 
@@ -6,9 +7,12 @@ const router = Router()
 
 const userController = new UserController()
 const surveysController = new SurveysController()
+const sendMail = new SendMailController
 
 router.post("/users", userController.create) // Rota de criação de usuário
 router.post("/surveys", surveysController.create) // Rota de criação de enquete
 router.get("/surveys", surveysController.show) // Rota que mosta todas as enquetes
+
+router.post("/sendMail", sendMail.execute)
 
 export { router }
